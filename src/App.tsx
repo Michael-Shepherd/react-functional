e71bf528174d95231515f704c6d915ed2a2ac185
Michael Shepherd (Glacier) <Michael.Shepherd@glacier.co.za>
2023-04-25 07:16:59 +0200

    Added routing and other exciting things

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import Board from "./components/Board";
 import ProjectsPage from "./projects/ProjectsPage";
 import './styles.css';
+import HomePage from "./home/HomePage";
+import ProjectPage from "./projects/ProjectPage";
 
 function App() {
   return (
-    <div>
-      <ProjectsPage/>
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/src/assets/react.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/projects" element={<ProjectsPage />}></Route>
+          <Route path="/projects/:id" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,22 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MOCK_PROJECTS } from './MockProjects'
 import ProjectsList from './ProjectsList';
 import { Project } from './Project';
+import { projectApi } from '../services/projectApi';
 
 const ProjectsPage = () => {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS)
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        async function loadProjects() {
+            setLoading(true);
+            try {
+                const projectsResponse = await projectApi.get(1, 1000);
+                setError('');
+                setProjects(projectsResponse);
+            } catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, []);
 
     const saveProject = (project: Project) => {
-        let updatedProjects = projects.map((p: Project) => {
-            return p.id === project.id ? project : p;
-        });
-        setProjects(updatedProjects)
+
+        projectApi.put(project)
+            .then((updatedProject: Project) => {
+                let updatedProjects = projects.map((p: Project) => {
+                    return p.id === project.id ? updatedProject : p;
+                });
+                setProjects(updatedProjects)
+            })
+            .catch((e) => {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            });
     };
 
     return (
         <>
             <h1>ProjectsPage</h1>
+            {
+                error &&
+                <div className='error-block'>
+                    <p><span className="icon-alert"></span>{error}</p>
+                </div>
+            }
             <ProjectsList projects={projects} onSave={saveProject} />
+            {
+                loading &&
+                <div className="center-page">
+                    <span className="spinner primary"></span>
+                    <p>Loading...</p>
+                </div>
+            }
         </>
     )
 }
